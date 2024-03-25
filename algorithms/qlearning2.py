f4053d7a0382e79ecf1d170dfee31c90160d3129
alliajagbe <alliajagbe001@gmail.com>
2024-03-25 18:34:34 +0530

    qlearning

--- algorithms/qlearning2.py
+++ algorithms/qlearning2.py
@@ -1,107 +1,76 @@
-import gymnasium as gym
+import gym
 import numpy as np
-import matplotlib.pyplot as plt
-import pickle
 
-def run(is_training=True, render=False):
+# Initialize the CartPole environment
+env = gym.make('CartPole-v1')
 
-    env = gym.make('CartPole-v1', render_mode='human' if render else None)
+# Discretize the state space
+num_buckets = (1, 1, 6, 3)  # (position, velocity, angle, angular velocity)
+num_actions = env.action_space.n
+Q_table = np.zeros(num_buckets + (num_actions,))
 
-    # Divide position, velocity, pole angle, and pole angular velocity into segments
-    pos_space = np.linspace(-2.4, 2.4, 10)
-    vel_space = np.linspace(-4, 4, 10)
-    ang_space = np.linspace(-.2095, .2095, 10)
-    ang_vel_space = np.linspace(-4, 4, 10)
+# Define the Q-learning parameters
+learning_rate = 0.1
+discount_factor = 0.99
+exploration_rate = 1.0
+exploration_decay_rate = 0.99
+min_exploration_rate = 0.01
 
-    if(is_training):
-        q = np.zeros((len(pos_space)+1, len(vel_space)+1, len(ang_space)+1, len(ang_vel_space)+1, env.action_space.n)) # init a 11x11x11x11x2 array
+# Discretize the state
+def discretize_state(state):
+    # Define lower and upper bounds as lists
+    lower_bounds = [env.observation_space.low[0], -0.5, env.observation_space.low[2], -np.radians(50)]
+    upper_bounds = [env.observation_space.high[0], 0.5, env.observation_space.high[2], np.radians(50)]
+    ratios = [(state[i] + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i]) for i in range(len(state))]
+    discrete_state = [int(round((num_buckets[i] - 1) * ratios[i])) for i in range(len(state))]
+    discrete_state = [min(num_buckets[i] - 1, max(0, discrete_state[i])) for i in range(len(state))]
+    return tuple(discrete_state)
+
+# Choose action using epsilon-greedy policy
+def choose_action(state, exploration_rate):
+    if np.random.random() < exploration_rate:
+        return env.action_space.sample()  # Explore action space
     else:
-        f = open('cartpole.pkl', 'rb')
-        q = pickle.load(f)
-        f.close()
+        return np.argmax(Q_table[state])  # Exploit learned values
 
-    learning_rate_a = 0.1 # alpha or learning rate
-    discount_factor_g = 0.99 # gamma or discount factor.
+# Update Q-values using Q-learning update rule
+def update_Q_value(state, action, reward, next_state, next_action):
+    best_next_action = np.argmax(Q_table[next_state])
+    target = reward + discount_factor * Q_table[next_state + (best_next_action,)]
+    Q_table[state + (action,)] += learning_rate * (target - Q_table[state + (action,)])
 
-    epsilon = 1         # 1 = 100% random actions
-    epsilon_decay_rate = 0.00001 # epsilon decay rate
-    rng = np.random.default_rng()   # random number generator
+# Train the agent
+num_episodes = 1000
+for episode in range(num_episodes):
+    state = discretize_state(env.reset())
+    exploration_rate = max(min_exploration_rate, exploration_rate * exploration_decay_rate)
 
-    rewards_per_episode = []
+    done = False
+    total_reward = 0
+    while not done:
+        action = choose_action(state, exploration_rate)
+        next_state, reward, done, _ = env.step(action)
+        next_state = discretize_state(next_state)
+        update_Q_value(state, action, reward, next_state, action)
+        state = next_state
+        total_reward += reward
 
-    i = 0
+    if episode % 100 == 0:
+        print(f"Episode {episode}, Total Reward: {total_reward}")
 
-    # for i in range(episodes):
-    while(True):
+# Test the trained agent
+total_rewards = []
+num_test_episodes = 100
+for _ in range(num_test_episodes):
+    state = discretize_state(env.reset())
+    done = False
+    total_reward = 0
+    while not done:
+        action = np.argmax(Q_table[state])
+        next_state, reward, done, _ = env.step(action)
+        next_state = discretize_state(next_state)
+        state = next_state
+        total_reward += reward
+    total_rewards.append(total_reward)
 
-        state = env.reset()[0]      # Starting position, starting velocity always 0
-        state_p = np.digitize(state[0], pos_space)
-        state_v = np.digitize(state[1], vel_space)
-        state_a = np.digitize(state[2], ang_space)
-        state_av = np.digitize(state[3], ang_vel_space)
-
-        terminated = False          # True when reached goal
-
-        rewards=0
-
-        while(not terminated and rewards < 10000):
-
-            if is_training and rng.random() < epsilon:
-                # Choose random action  (0=go left, 1=go right)
-                action = env.action_space.sample()
-            else:
-                action = np.argmax(q[state_p, state_v, state_a, state_av, :])
-
-            new_state,reward,terminated,_,_ = env.step(action)
-            new_state_p = np.digitize(new_state[0], pos_space)
-            new_state_v = np.digitize(new_state[1], vel_space)
-            new_state_a = np.digitize(new_state[2], ang_space)
-            new_state_av= np.digitize(new_state[3], ang_vel_space)
-
-            if is_training:
-                q[state_p, state_v, state_a, state_av, action] = q[state_p, state_v, state_a, state_av, action] + learning_rate_a * (
-                    reward + discount_factor_g*np.max(q[new_state_p, new_state_v, new_state_a, new_state_av,:]) - q[state_p, state_v, state_a, state_av, action]
-                )
-
-            state = new_state
-            state_p = new_state_p
-            state_v = new_state_v
-            state_a = new_state_a
-            state_av= new_state_av
-
-            rewards+=reward
-
-            if not is_training and rewards%100==0:
-                print(f'Episode: {i}  Rewards: {rewards}')
-
-        rewards_per_episode.append(rewards)
-        mean_rewards = np.mean(rewards_per_episode[len(rewards_per_episode)-100:])
-
-        if is_training and i%100==0:
-            print(f'Episode: {i} {rewards}  Epsilon: {epsilon:0.2f}  Mean Rewards {mean_rewards:0.1f}')
-
-        if mean_rewards>195:
-            break
-
-        epsilon = max(epsilon - epsilon_decay_rate, 0)
-
-        i+=1 
-
-    env.close()
-
-    # Save Q table to file
-    if is_training:
-        f = open('cartpole.pkl','wb')
-        pickle.dump(q, f)
-        f.close()
-
-    mean_rewards = []
-    for t in range(i):
-        mean_rewards.append(np.mean(rewards_per_episode[max(0, t-100):(t+1)]))
-    plt.plot(mean_rewards)
-    plt.savefig(f'cartpole.png')
-
-if __name__ == '__main__':
-    # run(is_training=True, render=False)
-
-    run(is_training=False, render=True)
+print(f"Average Total Reward: {np.mean(total_rewards)}")
