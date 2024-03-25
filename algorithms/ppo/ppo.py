32b1717e34a2284729fa5e16a9f1456739de4b4f
alliajagbe <alliajagbe001@gmail.com>
2024-03-25 23:51:02 +0530

    ppo output

--- algorithms/ppo/ppo.py
+++ algorithms/ppo/ppo.py
@@ -1,5 +1,5 @@
 '''
-This is a simple example of using the PPO algorithm to solve the CartPole-v1 environment from OpenAI Gym.
+This is an implementation of the PPO algorithm to solve the CartPole-v1 environment from OpenAI Gym.
 '''
 
 import gym
@@ -11,8 +11,8 @@ env_name = 'CartPole-v1'
 env = gym.make(env_name, render_mode='human')
 env = DummyVecEnv([lambda: env])
 model = PPO('MlpPolicy', env, verbose=1)
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=1000)
 
-print(evaluate_policy(model, env, n_eval_episodes=10))
+print(evaluate_policy(model, env, n_eval_episodes=100))
 
 env.close()
